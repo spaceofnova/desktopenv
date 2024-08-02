1a606263d1777c634a452ab8a3c4409167a417af
spaceofnova <mcrealms23@gmail.com>
2024-08-01 23:09:08 -0500

    Fuck you react types

--- src/providers/WindowManagerContext.ts
+++ src/providers/WindowManagerContext.ts
@@ -2,7 +2,7 @@ import React from "react";
 import { WindowType } from "@/types/WindowTypes";
 
 export const WindowManagerContext = React.createContext({
-  windows: [],
+  windows: [] as WindowType[],
   addWindow: (_windowID: string) => {},
   removeWindow: (_window: WindowType) => {},
   bringToFront: (_windowName: string) => {},
